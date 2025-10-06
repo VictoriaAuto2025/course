b2630f0912c89597e40aeef6129a2d4486ea9f9d
Viktoria Boldyreva <vboldyreva@inno.tech>
2025-10-06 21:39:37 +0300

    Доработка логики проекта

--- src/ru/stepup/demo/MainApp.java
+++ src/ru/stepup/demo/MainApp.java
@@ -4,6 +4,7 @@ public class MainApp {
         public static void main(String[] args) {
 
             System.out.println("Hello world!");
+            System.out.println("123456789");
 
         }
 
