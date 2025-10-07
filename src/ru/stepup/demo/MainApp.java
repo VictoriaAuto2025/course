35761a7b1298d34dc97c2ab4b8c54346031cd939
Viktoria Boldyreva <vboldyreva@inno.tech>
2025-10-07 20:20:02 +0300

    Доработка логики

--- src/ru/stepup/demo/MainApp.java
+++ src/ru/stepup/demo/MainApp.java
@@ -4,7 +4,7 @@ public class MainApp {
         public static void main(String[] args) {
 
             System.out.println("Hello world!");
-            System.out.println("12345678");
+            System.out.println("18885678");
 
         }
 
